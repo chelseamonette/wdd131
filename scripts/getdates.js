007e9f87c31300d242625eb73e2d6ae85945dedb
chelseamonette <mon24045@byui.edu>
2025-01-13 14:35:51 -0800

    Fixed layout to align with example and fixed javascript

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,4 +1,5 @@
-const year = document.querySelector("#year");
+var d0 = new Date();
+document.getElementById("year").innerHTML = d0.getFullYear();
 
 
 let lastModified = new Date(document.lastModified);
