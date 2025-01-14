0b1b8c02050cfb9316549328d68715ae91ec5d22
chelseamonette <mon24045@byui.edu>
2025-01-13 17:47:09 -0800

    finally fixed the date modified issue I was having.  Found the answer on w3schools.com

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,5 +1,6 @@
 var d0 = new Date();
 document.getElementById("year").innerHTML = d0.getFullYear();
 
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
 
-let lastModified = new Date(document.lastModified);
