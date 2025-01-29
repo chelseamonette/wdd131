7cf36f4e15f6306e036f4f6ec0cfd7b36199c694
chelseamonette <mon24045@byui.edu>
2025-01-29 14:01:25 -0800

    finished working with both css pages.

--- scripts/place.js
+++ scripts/place.js
@@ -4,3 +4,8 @@ document.getElementById("year").innerHTML = d0.getFullYear();
 
 let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
+
+function calculateWindChill(temperature, windSpeed){
+
+    return 35.74 + (0.6215 * temperature) - (35.75*(windSpeed**0.16)) + ((0.4275*temperature)*(windSpeed**0.16));
+}
