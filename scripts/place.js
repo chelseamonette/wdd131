316e4f4037a50f3ee71c242253f78a366fc24789
chelseamonette <mon24045@byui.edu>
2025-01-29 16:27:12 -0800

    finished the javascript

--- scripts/place.js
+++ scripts/place.js
@@ -6,6 +6,17 @@ let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
 
 function calculateWindChill(temperature, windSpeed){
+    if(temperature <= 10 && windSpeed > 4.8){
 
-    return 35.74 + (0.6215 * temperature) - (35.75*(windSpeed**0.16)) + ((0.4275*temperature)*(windSpeed**0.16));
+        windChill = 13.12 + (0.6215 * temperature) - (11.37*(windSpeed**0.16)) + ((0.3965*temperature)*(windSpeed**0.16));
+        return `${Math.round(windChill)} &deg;C`;
+    }
+    else{
+        return "N/A";
+    }
 }
+
+let temperature = parseFloat(document.querySelector("#temperature").textContent);
+let windSpeed = parseFloat(document.querySelector("#windSpeed").textContent);
+
+document.getElementById("windChill").innerHTML = calculateWindChill(temperature, windSpeed);
