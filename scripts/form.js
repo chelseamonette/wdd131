0619403895d29d6c6a3c21c5ff49d3be35eda3a1
chelseamonette <mon24045@byui.edu>
2025-02-13 02:23:42 -0800

    added more style to the star ratings

--- scripts/form.js
+++ scripts/form.js
@@ -4,3 +4,35 @@ document.getElementById("year").innerHTML = d0.getFullYear();
 
 let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
+
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+products.forEach(product => {
+    const productName = document.querySelector
+});
