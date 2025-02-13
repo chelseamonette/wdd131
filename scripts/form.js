d9c1361e42951166751a5a99241fb7c3d0b115e5
chelseamonette <mon24045@byui.edu>
2025-02-13 01:37:48 -0800

    form created

---
+++ scripts/form.js
@@ -0,0 +1,6 @@
+// Footer dynamics
+var d0 = new Date();
+document.getElementById("year").innerHTML = d0.getFullYear();
+
+let text = document.lastModified;
+document.getElementById("lastModified").innerHTML = text;
